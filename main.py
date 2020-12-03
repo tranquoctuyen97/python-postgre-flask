85bac7bdfc733dbdf9d1421e1fe79b1ff1013f18
Tran Quoc Tuyen <tuyentq.1997@gmail.com>
2020-12-03 21:49:57 +0700

    Remove unused code.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
-from json import JSONEncoder
 from sqlalchemy import or_, DateTime
 import bcrypt
 import jwt
